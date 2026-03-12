055235456c7ae81025b32e65de391d2ad6cf404f
ProAir-Climate-Change <contact@proairuk.co.uk>
2026-03-12 21:20:44 +0000

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,12 +1,1 @@
-export const metadata = {
-  title: "ProAir Tools",
-  description: "ProAir room sizing calculator",
-};
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0 }}>{children}</body>
-    </html>
-  );
-}
