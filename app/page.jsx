408b7d3c26158eda404ba9bf311c07b4ba73535a
ProAir-Climate-Change <contact@proairuk.co.uk>
2026-03-23 17:24:02 +0000

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -253,7 +253,7 @@ const height = Number(room.height);
 if (!room.length || !room.width) {
   return {
     ...room,
-    area: "",
+    area: "Enter room size to calculate",
     kw: "",
     recommended: "",
     recommendedNumber: 0,
